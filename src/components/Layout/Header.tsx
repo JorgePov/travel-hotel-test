a49398f5d4dfba56ea5ab103bbc6f8623311ba37
Jorge Poveda <jpoveda731@gmail.com>
2023-10-08 14:45:43 -0500

    Refactor: Change filter to comp, change icons properties

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,17 +1,14 @@
-import { Avatar, Center, Flex, Heading, Spacer, Text, Box, Button, Input } from '@chakra-ui/react';
-import { BedIcon, FlyIcon, Person } from '../../icons/CustomIcons'
+import { Avatar, Center, Flex, Heading, Spacer, Text, Box } from '@chakra-ui/react';
+import { BedIcon, FlyIcon } from '../shared/icons/CustomIcons'
 import { Link } from 'react-router-dom'
-import ReactDatePicker from 'react-datepicker';
-import { useState } from 'react';
-import './Header.css'
+
+import { Filters } from '../shared/Filters';
 
 
 export default function Header() {
-    const [startDate, setStartDate] = useState<Date>();
-    const [endDate, setEndDate] = useState<Date>();
+
     return (
         <header style={{ background: '#003b95', padding: '8px 0px 8px 0px' }}>
-
             <nav style={{ margin: '0 auto', maxWidth: '1000px' }}>
                 <Flex justifyContent={'center'} alignItems={'center'} padding={'8px 0px 16px 0px'} >
                     <Center>
@@ -21,13 +18,13 @@ export default function Header() {
                     <Avatar size={'sm'} />
                 </Flex>
 
-                <ul style={{ listStyle: 'none', color:'#fff' }}>
+                <ul style={{ listStyle: 'none', color: '#fff' }}>
                     <li style={{ display: 'flex', textAlign: 'center', userSelect: 'none', gap: '15px' }}>
                         <Link to={'/dashboard'} style={{ display: 'flex' }}>
                             <BedIcon width={24} height={24} fill='#fff' style={{ marginInlineEnd: '8px' }} />
                             Mis resrervas
                         </Link>
-                        <Link  to={'/dashboard'} style={{ display: 'flex' }}>
+                        <Link to={'/dashboard'} style={{ display: 'flex' }}>
                             <FlyIcon width={24} height={24} fill='#fff' style={{ marginInlineEnd: '8px' }} />
                             Reservar
                         </Link>
@@ -39,68 +36,8 @@ export default function Header() {
                     </Heading>
                     <Text fontSize='2xl'>Busca ofertas en hoteles, casas y mucho más...</Text>
                 </Box>
-
-                <Box
-                    left={'50%'}
-                    position={'absolute'}
-                    zIndex={'100'}
-                    maxWidth={'1100px'}
-                    width={'calc(100% - 10px)'}
-                    transform={'translate(-50%,-25px)'}
-                >
-                    <form style={{ margin: 0, padding: 0 }}>
-                        <Flex
-                            bg='secundary.0'
-                            padding={'4px'}
-                            borderRadius={'8px'}
-                            boxShadow={'0 2px 8px 0 rgba(26,26,26,0.16)'}
-                            gap={'3px'}
-                        >
-                            <Box bg='fontColor.white' flex={'1 1 auto'} borderRadius={'8px'}>
-                                <Flex padding={'8px'} alignItems={'center'}>
-                                    <BedIcon width={24} height={24} fill='#000' style={{ marginInlineEnd: '8px' }} />
-                                    <Input placeholder='¿A donde vas?' width={'100%'} size='md' color={'black'} />
-                                </Flex>
-                            </Box>
-                            <Box bg='fontColor.white' flex={'1 1 auto'} borderRadius={'8px'}>
-                                <Flex padding={'8px'} alignItems={'center'} >
-                                    <BedIcon width={42} height={42} fill='#000' style={{ marginInlineEnd: '8px' }} />
-                                    <ReactDatePicker
-                                        selected={startDate}
-                                        onChange={(date) => setStartDate(date!)}
-                                        selectsStart
-                                        startDate={startDate}
-                                        endDate={endDate}
-                                        placeholderText='Fecha Inicial'
-                                        calendarClassName={'width:100%'}
-                                        wrapperClassName='datePicker'
-                                    />
-                                    <ReactDatePicker
-                                        selected={endDate}
-                                        onChange={(date) => setEndDate(date!)}
-                                        selectsEnd
-                                        startDate={startDate}
-                                        endDate={endDate}
-                                        minDate={startDate}
-                                        placeholderText='Fecha Final'
-                                        wrapperClassName='datePicker'
-
-                                    />
-                                </Flex>
-                            </Box>
-                            <Box bg='fontColor.white' flex={'1 1 auto'} borderRadius={'8px'}>
-                                <Flex padding={'8px'} alignItems={'center'} >
-                                    <Person width={24} height={24} fill='#000' style={{ marginInlineEnd: '8px' }} />
-                                    <Input placeholder='Cuantas personas' width={'100%'} size='md' color={'black'} />
-                                </Flex>
-                            </Box>
-                            <Button height={''}>Buscar</Button>
-                        </Flex>
-                    </form>
-                </Box>
-
             </nav>
-
+            <Filters />
         </header>
     )
 }
---
+++ src/components/shared/Filters.tsx
@@ -0,0 +1,70 @@
+import { Box, Button, Flex, Input } from '@chakra-ui/react';
+import React, { useState } from 'react'
+import { BedIcon, Person, Shcedule } from './icons/CustomIcons';
+import ReactDatePicker from 'react-datepicker';
+import './Filters.css'
+
+export const Filters = () => {
+  const [startDate, setStartDate] = useState<Date>();
+  const [endDate, setEndDate] = useState<Date>();
+  return (
+    <Box
+      left={'50%'}
+      position={'absolute'}
+      zIndex={'100'}
+      maxWidth={'1100px'}
+      width={'calc(100% - 10px)'}
+      transform={'translate(-50%,-25px)'}
+    >
+      <form style={{ margin: 0, padding: 0 }}>
+        <Flex
+          bg='secundary.0'
+          padding={'4px'}
+          borderRadius={'8px'}
+          boxShadow={'0 2px 8px 0 rgba(26,26,26,0.16)'}
+          gap={'3px'}
+        >
+          <Box bg='fontColor.white' flex={'1 1 auto'} borderRadius={'8px'}>
+            <Flex padding={'8px'} alignItems={'center'}>
+              <BedIcon width={24} height={24} fill='#000' style={{ marginInlineEnd: '8px' }} />
+              <Input placeholder='¿A donde vas?' width={'100%'} size='md' color={'black'} />
+            </Flex>
+          </Box>
+          <Box bg='fontColor.white' flex={'1 1 auto'} borderRadius={'8px'}>
+            <Flex padding={'8px'} alignItems={'center'} >
+              <Shcedule width={42} height={42} fill='#000' style={{ marginInlineEnd: '8px' }} />
+              <ReactDatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date!)}
+                selectsStart
+                startDate={startDate}
+                endDate={endDate}
+                placeholderText='Fecha Inicial'
+                calendarClassName={'width:100%'}
+                wrapperClassName='datePicker'
+              />
+              <ReactDatePicker
+                selected={endDate}
+                onChange={(date) => setEndDate(date!)}
+                selectsEnd
+                startDate={startDate}
+                endDate={endDate}
+                minDate={startDate}
+                placeholderText='Fecha Final'
+                wrapperClassName='datePicker'
+
+              />
+            </Flex>
+          </Box>
+          <Box bg='fontColor.white' flex={'1 1 auto'} borderRadius={'8px'}>
+            <Flex padding={'8px'} alignItems={'center'} >
+              <Person width={24} height={24} fill='#000' style={{ marginInlineEnd: '8px' }} />
+              <Input placeholder='Cuantas personas' width={'100%'} size='md' color={'black'} />
+            </Flex>
+          </Box>
+          <Button height={''}>Buscar</Button>
+        </Flex>
+      </form>
+    </Box>
+  )
+}
